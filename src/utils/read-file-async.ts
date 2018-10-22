13f6d2dd68bbb8eadec47d5fe6f26a9076678408
Lukáš Rada <rada@havit.cz>
2018-10-22 18:34:20 +0200

    #1 FileReader.onload doesn't have return

--- src/utils/read-file-async.ts
+++ src/utils/read-file-async.ts
@@ -1,17 +1,16 @@
 export const readFileAsync = (file: File) => {
-    const fileReader = new FileReader();
+    const fileReader = new FileReader()
   
     return new Promise((resolve: (value: number[]) => void, reject) => {
         fileReader.onerror = () => {
-        fileReader.abort();
-        reject(new Error("Problem parsing input file."));
-      };
+        fileReader.abort()
+        reject(new Error("Problem parsing input file."))
+      }
   
       fileReader.onload = () => {
         const result = Array.from(new Uint8Array(fileReader.result as ArrayBuffer))         
-        resolve(result);
-        return result
-      };
-      fileReader.readAsArrayBuffer(file);
-    });
-  };
+        resolve(result)
+      }
+      fileReader.readAsArrayBuffer(file)
+    })
+  }
