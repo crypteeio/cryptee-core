3dbe56f17c20cdea7159158626029fd5d970e1cb
Lukáš Rada <rada@havit.cz>
2018-10-26 22:42:02 +0200

    v1.0.6

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
+import { encodeText } from './services/encode-text'
 import { encryptText } from './services/encrypt-text'
+import { decodeText } from './services/decode-text'
 import { decryptText } from './services/decrypt-text'
+import { encodeFile } from './services/encode-file'
 import { encryptFile } from './services/encrypt-file'
+import { decodeFile } from './services/decode-file'
 import { decryptFile } from './services/decrypt-file'
 
 export {
+    encodeText,
     encryptText,
+    decodeText,
     decryptText,
+    encodeFile,
     encryptFile,
+    decodeFile,
     decryptFile,
 }
